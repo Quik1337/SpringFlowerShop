6f02891f9f489e759141b509ca5223112914720c
David Sodergren <david.sodergren94@gmail.com>
2019-05-20 11:20:00 +0200

    first commit

---
+++ src/test/java/flowershop/springproject/flowershop/FlowershopApplicationTests.java
@@ -0,0 +1,16 @@
+package flowershop.springproject.flowershop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlowershopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
