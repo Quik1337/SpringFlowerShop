6140eb96349edc294d2e13a0a89d22504faf3d83
David Sodergren <david.sodergren94@gmail.com>
2019-05-21 11:40:51 +0200

    commit innan pull from adam

--- src/main/java/flowershop/springproject/flowershop/repositories/CustomerRepository.java
+++ src/main/java/flowershop/springproject/flowershop/repositories/CustomerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     Customer findByEmailAndPassword(String email, String password);
+
+
 }
