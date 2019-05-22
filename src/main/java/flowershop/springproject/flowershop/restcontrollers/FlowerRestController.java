5e9feb7794888b0df9a0b1bedc57e0a7e938bd75
David Sodergren <david.sodergren94@gmail.com>
2019-05-22 11:30:18 +0200

    alla rest api services klara /ds & /ab

---
+++ src/main/java/flowershop/springproject/flowershop/restcontrollers/FlowerRestController.java
@@ -0,0 +1,27 @@
+package flowershop.springproject.flowershop.restcontrollers;
+
+import flowershop.springproject.flowershop.models.Flower;
+import flowershop.springproject.flowershop.services.FlowerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("flower")
+public class FlowerRestController {
+
+    private final FlowerService flowerService;
+
+    public FlowerRestController(FlowerService flowerService) {
+        this.flowerService = flowerService;
+    }
+
+    @GetMapping("getAllFlowers")
+    public @ResponseBody Set<Flower> getAllFlowers(){
+        return flowerService.getAll();
+    }
+
+}
